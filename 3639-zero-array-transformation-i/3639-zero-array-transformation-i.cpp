292e784689079a1c3fa95fe0565f0ac6610275d3
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-05-20 20:06:21 +0530

    Time: 137 ms (27.24%), Space: 325.4 MB (22.43%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+
+        vector<int> diffArr(n, 0);
+
+        for(auto q : queries) {
+            int start = q[0];
+            int end = q[1];
+
+            diffArr[start] += 1;
+            if(end + 1 < n){
+                diffArr[end+1] -= 1;
+            }
+        }
+
+        // vector<int> arr(n, 0);
+        // int cumSum = diffArr[0];
+        // arr[0] = diffArr[0];
+
+        // for(int i=1;i<n;i++){
+        //     cumSum += diffArr[i];
+        //     arr[i] = cumSum;
+        // }
+        //taking cummulative sum in diffArr
+        for(int i=1;i<n;i++){
+            diffArr[i] += diffArr[i-1];
+        }
+
+        for(int i=0;i<n;i++){
+            if(nums[i] - diffArr[i] > 0) return false;
+        }
+
+        return true;
+    }
+};

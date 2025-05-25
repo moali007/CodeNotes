592852e55eeb0d79e89a815aff720373422f221a
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-05-25 23:10:50 +0530

    Time: 83 ms (44.39%), Space: 61.7 MB (34.4%) - LeetHub

---
+++ 3860-resulting-string-after-adjacent-removals/3860-resulting-string-after-adjacent-removals.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    
+                // char ch = s[i];
+                // int idx = ch - 'a';
+
+                // int next_idx = idx + 1;
+                // if(next_idx > 25) next_idx -= 26;
+                // char next_char = 'a' + next_idx;
+
+                // int prev_idx = idx - 1;
+                // if(prev_idx < 0) prev_idx += 26;
+                // char prev_char = 'a' + prev_idx;
+
+    bool isAdjacent(char ch1, char ch2){
+        //checks if ch2 is adjacent char of ch1
+        int idx = ch1 - 'a';
+
+        int next_idx = idx + 1;
+        if(next_idx > 25) next_idx -= 26;
+        char next_char = 'a' + next_idx;
+
+        int prev_idx = idx - 1;
+        if(prev_idx < 0) prev_idx += 26;
+        char prev_char = 'a' + prev_idx;
+
+        return (ch2 == next_char || ch2 == prev_char);
+    }
+
+    string resultingString(string s) {
+
+        int n = s.length();
+        stack<char> st;
+        st.push(s[0]);
+
+        for(int i=1;i<n;i++){
+           char ch1 = s[i];
+
+           if(!st.empty()){
+              char ch2 = st.top();
+              if(isAdjacent(ch2, ch1) == true){
+                st.pop();
+              }else{
+                st.push(ch1);
+              }
+           }
+           else{
+            st.push(ch1);
+           }
+        }
+
+        if(st.empty()) return "";
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};

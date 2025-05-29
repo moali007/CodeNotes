d066bfcd1ebc6f00eab2533b82f4756a9859fed4
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-05-29 21:44:47 +0530

    Time: 707 ms (11.56%), Space: 424.1 MB (6.12%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -16,27 +16,27 @@ public:
         return adj;
     }
 
-    void dfs(int node, int zero, vector<bool>&vis, vector<int>&mark, unordered_map<int, vector<int>>&adj){
+    void dfs(int node, int val, vector<bool>&vis, vector<int>&mark, unordered_map<int, vector<int>>&adj){
         vis[node] = true;
-        mark[node] = zero;
+        mark[node] = val;
 
         for(auto it : adj[node]){
-            if(!vis[it]){
-                dfs(it, !zero, vis, mark, adj);
+            if(!vis[it]){ 
+                dfs(it, !val, vis, mark, adj); //this dfs will mark the neighbours of current node with alternate of what current node was marked
             }
         }
     }
 
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
         int N = edges1.size() + 1; //number of nodes in tree 1
-        int M = edges2.size() + 1;
+        int M = edges2.size() + 1; ////number of nodes in tree 2
 
-        unordered_map<int, vector<int>> adj1 = findAdjList(edges1);
-        unordered_map<int, vector<int>> adj2 = findAdjList(edges2);
+        unordered_map<int, vector<int>> adj1 = findAdjList(edges1); //adj1 = adjacency List of tree1
+        unordered_map<int, vector<int>> adj2 = findAdjList(edges2); //adj2 = adjacency List of tree2
 
         vector<int> mark(N, -1);
         vector<bool> vis(N, false);
-        int zero = 0;
+        int zero = 0; //marking the starting node of tree1 with 0
         for(int i=0;i<N;i++){
             if(!vis[i]){
                 dfs(i, zero, vis, mark, adj1);
@@ -77,8 +77,6 @@ public:
         }
 
         int maxValue_In_tree2 = max(zeroCnt, oneCnt);
-
-        // vector<int> result2(M);
 
         for(int i=0;i<N;i++){
             result1[i] += maxValue_In_tree2;

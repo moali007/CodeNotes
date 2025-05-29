013b64205b22ccd4af9e7774ae4b071cd4eec325
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-05-29 23:42:15 +0530

    Time: 8 ms (80.29%), Space: 40.3 MB (91.18%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         int n=profit.size(), m=worker.size();
-        vector<pair<int,int>> d_p(n);
-        for(int i=0; i<n; i++) 
-            d_p[i]={difficulty[i], profit[i]};
 
-        sort(d_p.begin(), d_p.end());
+        vector<pair<int,int>> temp(n);
+
+        for(int i=0; i<n; i++){
+            temp[i]={difficulty[i], profit[i]};
+        }
+
+        sort(temp.begin(), temp.end());
         sort(worker.begin(), worker.end());
         
         int maxProfit=0,  idx=0, bestProfit = 0;
 
         for (int i = 0; i < m; i++) {
             // Increase index to the next worker's ability
-            while (idx < n && worker[i] >= d_p[idx].first) {
-                bestProfit = max(bestProfit, d_p[idx].second);
+            while (idx < n && worker[i] >= temp[idx].first) {
+                bestProfit = max(bestProfit, temp[idx].second);
                 idx++;
             }
             maxProfit+= bestProfit;

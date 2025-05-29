b5d7652503111888fca66d08353df3ac752bfc3e
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-05-30 01:12:42 +0530

    Time: 10 ms (28.15%), Space: 21.2 MB (12.31%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+         unordered_map<int, vector<int>> adj;
+        
+        for(auto it : prerequisites){
+            int u = it[1];
+            int v = it[0];
+            
+            adj[u].push_back(v);
+        }
+        
+        vector<int> indegree(n, 0);
+        
+        for(int i=0;i<n;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        
+        vector<int> ans;
+        
+        queue<int> q;
+        
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it : adj[node]){
+                indegree[it]--;
+                
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        
+        return ans.size() == n;
+    }
+};

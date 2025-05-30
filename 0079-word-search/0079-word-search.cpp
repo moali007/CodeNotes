7e424874576f8865894fa020da6e901e08eecca0
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-05-30 23:52:07 +0530

    Time: 298 ms (59.41%), Space: 10.4 MB (94.35%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int m,n;
+
+    bool find(int row, int col, int idx, string &word, vector<vector<char>>& board, int delrow[], int delcol[]){
+        if(idx == word.length()){
+            return true;
+        }
+
+        if(row<0 || col<0 || row>=m || col>=n || board[row][col] == '$'){
+            return false;
+        }
+
+        if(board[row][col] != word[idx]){
+            return false;
+        }
+
+        //yaha aa gae matlab board[row][col] == word[idx] --> visit board[row][col]
+
+        //do
+        char temp = board[row][col];
+        board[row][col] = '$';
+
+        for(int i=0;i<4;i++){
+            int drow = row + delrow[i];
+            int dcol = col + delcol[i];
+
+            if(find(drow, dcol, idx+1, word, board, delrow, delcol) == true){
+                return true;
+            }
+        }
+
+        //undo
+        board[row][col] = temp;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        m = board.size();
+        n = board[0].size();
+
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(board[i][j] == word[0] && find(i, j, 0, word, board, delrow, delcol) == true){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};

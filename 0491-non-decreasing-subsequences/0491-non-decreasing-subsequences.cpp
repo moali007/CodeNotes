cba901ad919b9cf25810b3d30797abc3d1dff1bd
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-05-31 22:47:23 +0530

    Time: 81 ms (21.68%), Space: 48.9 MB (22.63%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    vector<vector<int>> ans;
+    
+    void f(int i, vector<int>& nums, vector<int>& arr){
+        if(i == nums.size()){
+            return;
+        }
+
+        //not take
+        f(i+1, nums, arr);
+
+        //take
+        if(arr.size() == 0 || nums[i] >= arr.back()){
+            arr.push_back(nums[i]); //take //do
+
+            if(arr.size() >= 2){
+                ans.push_back(arr);
+        
+                // f(i+1, nums, arr);
+            }
+            f(i+1, nums, arr);
+
+            arr.pop_back(); //undo
+        }
+    }
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<int> arr;
+
+        f(0, nums, arr);
+
+        set<vector<int>> st(ans.begin(), ans.end());
+        // for(auto it : ans){
+        //     st.insert(it);
+        // }
+        ans.clear();
+        for(auto it : st){
+            ans.push_back(it);
+        }
+
+        return ans;
+    }
+};

4d1f79d1dac77cde2dae66945ffac65f834e21f7
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-01 22:52:26 +0530

    Time: 42 ms (5.11%), Space: 8.4 MB (90.34%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.cpp
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.cpp
@@ -3,11 +3,11 @@ public:
     
     int solve(int childCount, int n, int limit){
         if(childCount == 3){
-            if(n == 0) return 1;
-
+            if(n == 0){
+                return 1;
+            }
             return 0;
         }
-        // if((childCount == 3 && n != 0) || (n == 0 && childCount != 3)) return 0;
 
         int ways = 0;
         for(int assign=0;assign<=limit;assign++){

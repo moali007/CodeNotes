0b32874609c246013e2d0e7eb46a20b0a1e0cf97
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-01 21:28:58 +0530

    Time: 63 ms (24.34%), Space: 50.9 MB (5.55%) - LeetHub

---
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/3884-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> ans(m-k+1, vector<int>(n-k+1, 0));
+        
+        for(int i = 0; i <= m-k; i++){
+            for(int j = 0; j <= n-k; j++){
+
+                set<int> unique;
+
+                for(int x = i; x < i+k; x++){
+                    for(int y = j; y < j+k; y++){
+
+                        unique.insert(grid[x][y]);
+                    }
+                }
+
+                if(unique.size() <= 1) ans[i][j] = 0;
+                else{
+                    vector<int> vec;
+                    for(auto it : unique){
+                        vec.push_back(it);
+                    }
+                    //vec will be sorted, bas minn diff nikal lo
+                    int minDiff = INT_MAX;
+                    for(int z = 1; z < vec.size(); z++){
+                        minDiff = min(minDiff, vec[z]-vec[z-1]);
+                    }
+
+                    ans[i][j] = minDiff;
+                }
+            }
+        }
+
+        return ans;
+
+    }
+};

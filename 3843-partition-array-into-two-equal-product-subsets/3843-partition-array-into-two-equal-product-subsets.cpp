4fc18efa6021a77f1f862d2f5075ac3958e43d56
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-01 19:35:14 +0530

    Time: 0 ms (100%), Space: 23.9 MB (28.96%) - LeetHub

--- 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
+++ 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool solve(int curr, long long mul, int n, vector<int> &nums, long long target, unordered_set<int> &first) {
-        if (curr >= n) return false;    // index out of bound
+    bool solve(int i, long long mul, int n, vector<int> &nums, long long target, unordered_set<int> &first) {
+        if (i >= n) return false;    // index out of bound
         if (mul == target) return true; // current product equals target
         if (mul > target) return false; // we can never go back to smaller value
 
-        bool take = solve(curr + 1, mul * nums[curr], n, nums, target, first); 
-        // curr + 1 :           move to next element
-        // mul * nums[curr] :   since we took current element, we multiplied it to previous product
+        bool take = solve(i + 1, mul * nums[i], n, nums, target, first); 
+        // i + 1 :           move to next element
+        // mul * nums[i] :   since we took current element, we multiplied it to previous product
 
         // if we got the target by taking current element we push current index in the set and return true
         if (take) { 
-            first.insert(curr); 
+            first.insert(i); 
             return true; 
         }
-        bool notTake = solve(curr + 1, mul, n, nums, target, first); 
+        bool notTake = solve(i + 1, mul, n, nums, target, first); 
         // curr + 1 :   move to next element
         // mul :        since we did not take the current element, we pass the previous product to next call
         return notTake; 
@@ -23,16 +23,16 @@ public:
     bool checkEqualPartitions(vector<int>& nums, long long target) {
         int n = nums.size(); 
         unordered_set<int> first;
-        if (solve(0, 1, n, nums, target, first)) {  // got the target using some elements
-            if (first.size() == 0) return false;    // no elements left for second partition
+        if (solve(0, 1, n, nums, target, first)) { 
+            if (first.size() == 0) return false;   
 
             long long part2 = 1; 
-            for (int i = 0; i < n; i++) {   // taking product of remaining elemnts
+            for (int i = 0; i < n; i++) {  
                 if (first.count(i)) continue; 
                 part2 *= nums[i]; 
             } 
-            if (part2 == target) return true; // both partitions have product = target, we return true
+            if (part2 == target) return true; 
         }
-        return false; // false otherwhise
+        return false; 
     }
 };

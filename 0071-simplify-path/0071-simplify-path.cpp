7066d520bdae5a150a13503a269f737b2a29713c
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-07 00:09:47 +0530

    Time: 0 ms (100%), Space: 12.1 MB (16.23%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -4,26 +4,30 @@ public:
         
         stringstream ss(path);
         string token = "";
+
         stack<string> st;
 
         while(getline(ss, token, '/')){
             if(token == "." || token == "") continue;
 
-            if(token != "..") st.push(token);
-            else if(!st.empty()) st.pop();
+            if(token != ".."){
+                st.push(token);
+            }
+            else if(!st.empty()){
+                st.pop();
+            }
         }
-        
+
         if(st.empty()) return "/";
 
         string ans = "";
-        
+
         while(!st.empty()){
+
             ans = "/" + st.top() + ans;
             st.pop();
         }
 
         return ans;
-
-
     }
 };

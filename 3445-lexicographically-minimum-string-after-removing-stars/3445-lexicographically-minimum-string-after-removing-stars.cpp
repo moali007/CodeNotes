af22c673b371f95ba037c7e8fcaf3820010fb490
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-07 22:26:02 +0530

    Time: 162 ms (32.47%), Space: 26.1 MB (73.7%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    typedef pair<char, int> P;
+    
+    class compare{
+        public:
+        bool operator()(P &p1, P &p2){
+            if(p1.first == p2.first){
+                return p1.second < p2.second;
+            }
+            return p1.first > p2.first;
+        }
+    };
+
+    string clearStars(string s) {
+        int n = s.length();
+
+        priority_queue<P, vector<P>, compare> pq;
+
+        for(int i=0;i<n;i++){
+            if(s[i] != '*'){
+                pq.push({s[i], i});
+            }
+            else{
+                int idx = pq.top().second;
+                pq.pop();
+                s[idx] = '*';
+            }
+        }
+
+        string ans = "";
+        for(int i=0;i<n;i++){
+            if(s[i] != '*'){
+                ans.push_back(s[i]);
+            }
+        }
+
+        return ans;
+    }
+};

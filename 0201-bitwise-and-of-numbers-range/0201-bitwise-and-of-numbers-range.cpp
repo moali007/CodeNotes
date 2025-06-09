54aea21e0fb8fb762b2d420eef0996b9656a2f26
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-09 21:56:55 +0530

    Time: 2 ms (35.4%), Space: 11.2 MB (46.53%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        int shiftCnt = 0;
+        // int shiftCnt = 0;
 
-        while(left != right){
-            //longest common prefix
-            left = (left >> 1);
-            right = (right >> 1);
-            shiftCnt++;
+        // while(left != right){
+        //     //longest common prefix
+        //     left = (left >> 1);
+        //     right = (right >> 1);
+        //     shiftCnt++;
+        // }
+
+        // return (left << shiftCnt);
+
+        while(right > left){
+            right = (right & (right - 1));
         }
 
-        return (left << shiftCnt);
+        return right;
     }
 };

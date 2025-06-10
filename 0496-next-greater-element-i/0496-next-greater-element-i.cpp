03d26e0b0af84279f2fe37e12566fc7aceba256f
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-10 20:55:43 +0530

    Time: 0 ms (100%), Space: 13.1 MB (15.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,21 +4,19 @@ public:
         int n = nums2.size();
 
         vector<int> nge2(n); //next greater element of nums2
-
+         
         stack<int> st;
+        unordered_map<int, int> mp; //{nums2[i] - nge2[i]}
+
         for(int i=n-1;i>=0;i--){
             while(!st.empty() && st.top() <= nums2[i]){
                 st.pop();
             }
 
             nge2[i] = st.empty() ? -1 : st.top();
+            mp[nums2[i]] = nge2[i];
 
             st.push(nums2[i]);
-        }
-
-        unordered_map<int, int> mp; //{nums2[i] - nge2[i]}
-        for(int i=0;i<n;i++){
-            mp[nums2[i]] = nge2[i];
         }
 
         int m = nums1.size();

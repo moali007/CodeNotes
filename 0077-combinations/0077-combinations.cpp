1e2d0b097a368503eb1e702d70b9da183bd31d88
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-13 00:30:55 +0530

    Time: 97 ms (21.79%), Space: 108.9 MB (31.69%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -18,10 +18,27 @@ public:
         solve(start+1, n, k, temp);
     }
 
+    void solve1(int start, int n, int k, vector<int> &temp){
+        if(k == 0){
+            result.push_back(temp);
+            return;
+        }
+
+        for(int i=start ; i<=n ; i++){
+            temp.push_back(i);
+
+            solve(i+1, n, k-1, temp);
+
+            temp.pop_back();
+        }
+    
+    }
+
     vector<vector<int>> combine(int n, int k) {
 
         vector<int> temp;
-        solve(1, n, k, temp);
+        // solve(1, n, k, temp);
+        solve1(1, n, k, temp);
 
         return result;
     }

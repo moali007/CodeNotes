f1c63f20455a72032f0b56fc450a16ab3f4e8ba8
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-18 19:45:36 +0530

    Time: 0 ms (100%), Space: 25.6 MB (65.88%) - LeetHub

---
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        double minAvg = INT_MAX;
+
+        int i=0, j=n-1;
+
+        while(i < j){
+            double avg = (nums[i] + nums[j]) / 2.0;
+            minAvg = min(minAvg, avg);
+            i++, j--;
+        }
+
+        return minAvg;
+
+    }
+};

d842b28e2e75eaf46d86715b5028705936bff024
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-19 14:39:25 +0530

    Time: 0 ms (100%), Space: 9.3 MB (33.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,11 +10,31 @@ public:
             return 1; //ek valid path mil gya
         }
 
+        //koi grid hoti to mark kar deta curr path, but koi grid nahi to age badho
+
         int down = f(r+1, c, m, n);
         int right = f(r, c+1, m, n);
 
         return down + right;
 
+    }
+
+    int solve(int i, int j, int m,int n, vector<vector<int>> &dp){
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        if(i < 0 || j < 0){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int left = solve(i, j-1, m, n, dp);
+        int up = solve(i-1, j, m, n, dp);
+
+        return dp[i][j] = left+up;
     }
 
     int mem(int r, int c, int m, int n, vector<vector<int>> &dp){
@@ -39,7 +59,9 @@ public:
         //m -> row
         //n -> col
         // return f(0, 0 , m, n);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return mem(0, 0, m, n, dp);
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return mem(0, 0, m, n, dp);
+        return solve(m-1, n-1, m, n, dp);
     }
 };

f6f9f7ab5d1493f91afa3fcbc1323d153c451e4c
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-19 14:43:51 +0530

    Time: 0 ms (100%), Space: 11.9 MB (54.39%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int m,n;
-    int cnt;
-
     int f(int r, int c, vector<vector<int>>& grid, vector<vector<int>>& dp){
         if(r >= n || c >= m || grid[r][c] == 1){
             return 0;
         }
 
         if(r == n-1 && c == m-1){
-            // cnt += 1;
             return 1;
         }
 
@@ -19,12 +16,12 @@ public:
 
         //mtlb grid[r][c] == 0
 
-        grid[r][c] = 1; //mark
+        // grid[r][c] = 1; //mark
 
         int down = f(r+1, c, grid, dp);
         int right = f(r, c+1, grid, dp);
 
-        grid[r][c] = 0; //backtrack
+        // grid[r][c] = 0; //backtrack
 
         return dp[r][c] = down + right;
 
@@ -36,11 +33,7 @@ public:
 
         vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        cnt = 0;
-
         return f(0, 0, grid, dp);
-
-        // return cnt;
         
     }
 };

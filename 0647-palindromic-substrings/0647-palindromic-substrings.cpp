4518ec3d0e9c1e82677be50084d24e15807921aa
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-22 21:25:25 +0530

    Time: 1337 ms (7.98%), Space: 406.5 MB (10.23%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    bool isPallindrome(string str){
-        int i=0;
-        int j=str.length()-1;
+    bool isPallindrome(string str, int i, int j){
+        // int i=0;
+        // int j=str.length()-1;
 
         while(i < j){
             if(str[i] != str[j]) return false;
@@ -21,9 +21,8 @@ public:
         int cnt = 0;
         for(int i = 0; i<n;i++){
             for(int j=i;j<n;j++){
-                string str = s.substr(i, j-i+1);
-
-                if(isPallindrome(str)) cnt++;
+                int len = j-i+1;
+                if(isPallindrome(s, i, i + len - 1)) cnt++;
             }
         }
 

8534720db55f2e40c102a00917cf8fc5f0438389
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-22 21:28:58 +0530

    Time: 1336 ms (7.98%), Space: 405.8 MB (11.43%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -18,9 +18,9 @@ public:
     int countSubstrings(string s) {
         int n = s.length();
 
-        int cnt = 0;
+        int cnt = n;
         for(int i = 0; i<n;i++){
-            for(int j=i;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 int len = j-i+1;
                 if(isPallindrome(s, i, i + len - 1)) cnt++;
             }

4e6383fedf47eb025ee1064228256e166d31082b
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-06-26 21:32:59 +0530

    Time: 36 ms (17.46%), Space: 61 MB (9.52%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -9,47 +9,33 @@ public:
             p++;
             if(p >= 32) return false;
         }
+
         return val <= k;
     }
 
     int longestSubsequence(string s, int k) {
         int n = s.length();
 
-        // int zeros = 0;
-        // for(char ch : s){
-        //     if(ch == '0') zeros++;
-        // }
-        // int ans = zeros;
-        // int maxi = zeros;
-        int maxi = 0;
-        // int ones = 0;
-        // int zeros = 0;
+        int zeros = 0;
+        for(char ch : s){
+            if(ch == '0') zeros++;
+        }
+
+        int maxi = zeros;
+        int ones = 0;
 
         string str = "";
-        int strLen = 0;
-        int i;
-        for(i = n-1 ; i >= 0 ; i--){
-            if(s[i] == '0'){ 
-                str = '0' + str;
-                strLen++;
-            }
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(s[i] == '0') str = '0' + str;
             else{
-                // ones++;
+                ones++;
                 str = '1' + str;
                 if(check(str, k) == true){
-                    strLen++;
+                    maxi = max(maxi, zeros+ones);
                 }
             }
         }
 
-        maxi = max(maxi, strLen);
-
-        int cntZero = 0;
-        while(i >= 0){
-            if(s[i] == '0') cntZero++;
-        }
-
-        return maxi + cntZero;
+        return maxi;
     }
 };
-// 633,480,354

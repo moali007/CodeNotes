d5939ed5e6bb73b507b4fa532b8999531a9d1f66
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-07-01 02:08:45 +0530

    Time: 8 ms (89.78%), Space: 30 MB (93.41%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -4,33 +4,30 @@ public:
         int n = s.length();
         if(n % 2 != 0) return false;
 
-        stack<int> open; //stores index of open parenthesis when locked[i] = 1
-        stack<int> openClose; //stores of indexes of parethesis when locked[i] = 0
-
+        int open = 0;
         for(int i = 0; i < n; i++){
-            if(s[i] == '(' && locked[i] == '1'){
-                open.push(i);
-            }
-            else if((s[i] == '(' || s[i] == ')') && locked[i] == '0'){
-                openClose.push(i);
+            if(s[i] == '(' || locked[i] == '0'){
+                open++;
             }
             else if(s[i] == ')' && locked[i] == '1'){
-                //first check in open stack
-                if(!open.empty()){
-                    open.pop();
-                }
-                else if(!openClose.empty()){//then check in openClose stack
-                    openClose.pop();
-                }
-                else return false;
+                open--;
             }
+
+            if(open < 0) return false;
         }
 
-        while(!open.empty() && !openClose.empty() && open.top() < openClose.top()){
-            open.pop();
-            openClose.pop();
+        int close = 0;
+        for(int i = n - 1; i >= 0; i--){
+            if(s[i] == ')' || locked[i] == '0'){
+                close++;
+            }
+            else if(s[i] == '(' && locked[i] == '1'){
+                close--;
+            }
+
+            if(close < 0) return false;
         }
 
-        return open.empty();
+        return true;
     }
 };

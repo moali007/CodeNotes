4540506b58b71886febaac42ecc8f6ec4ce51920
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-07-01 19:34:01 +0530

    Time: 11 ms (44.45%), Space: 14.6 MB (44.43%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -3,34 +3,50 @@ public:
     string minRemoveToMakeValid(string s) {
         int n = s.length();
 
-        stack<int> st;
-        unordered_set<int> deleteIdx;
-
+        string str = "";
+        //L to R : removing extra closing brackets
+        int open = 0;
         for(int i = 0; i < n; i++){
-            if(s[i] == '('){
-                st.push(i);
+            if(isalpha(s[i])){
+                str += s[i];
+            }
+            else if(s[i] == '('){
+                open++;
+                str += s[i];
             }
             else if(s[i] == ')'){
-                if(!st.empty()){
-                    st.pop();
+                if(open > 0){
+                    open--;
+                    str += s[i];
                 }else{
-                    deleteIdx.insert(i);
+                    continue;
                 }
             }
         }
 
-        while(!st.empty()){
-            deleteIdx.insert(st.top());
-            st.pop();
-        }
-
+        //R to L : removing extra open brackets from str
         string result = "";
-        for(int i = 0; i < n; i++){
-            if(deleteIdx.count(i)) continue;
-
-            result += s[i];
+        int close = 0;
+        for(int i = str.length() - 1; i >= 0; i--){
+            char ch = str[i];
+            if(isalpha(ch)){
+                result += ch;
+            }
+            else if(ch == ')'){
+                close++;
+                result += ch;
+            }
+            else if(ch == '('){
+                if(close > 0){
+                    close--;
+                    result += ch;
+                }else{
+                    continue;
+                }
+            }
         }
-
+        
+        reverse(result.begin(), result.end());
         return result;
     }
 };

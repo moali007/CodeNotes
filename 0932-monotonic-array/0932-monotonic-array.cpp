92a1fb5620ef55827c9bc99b22e0b39f91e1ac09
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-07-27 21:58:35 +0530

    Time: 3 ms (30.74%), Space: 100.3 MB (66.18%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    bool isIncreasing(vector<int>& nums){
+        for(int i=0;i<nums.size();i++){
+            if(i > 0 && nums[i] < nums[i-1]) return false;
+        }
+
+        return true;
+    }
+
+    bool isDecreasing(vector<int>& nums){
+        for(int i=0;i<nums.size();i++){
+            if(i > 0 && nums[i] > nums[i-1]) return false;
+        }
+
+        return true;
+    }
+
+    bool isMonotonic(vector<int>& nums) {
+        return isIncreasing(nums) || isDecreasing(nums);
+    }
+};

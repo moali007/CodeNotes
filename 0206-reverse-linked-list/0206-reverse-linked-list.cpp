f46b3ed498d8f8d5c4d24d70bbb67ab0f1d2f759
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-07-29 21:17:04 +0530

    Time: 0 ms (100%), Space: 13.4 MB (40.39%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            ListNode* front = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = front;
+        }
+
+        return prev;
+    }
+
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(head == NULL || head->next == NULL) return head;
+
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+
+        ListNode* leftPart = dummy;
+        for(int i = 1; i < left; i++) {
+            leftPart = leftPart->next;
+        }
+
+        ListNode* l = leftPart->next;
+        ListNode* r = l;
+        for(int i = left; i < right; i++) {
+            r = r->next;
+        }
+
+        ListNode* rightPart = r->next;
+        r->next = NULL;
+
+        ListNode* reversedHead = reverseList(l);
+        leftPart->next = reversedHead;
+        l->next = rightPart;
+
+        return dummy->next;
+    }
+};

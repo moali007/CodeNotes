ab0f0751424f7a4cbef166cd70a9e58d80daa419
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-08-11 01:20:10 +0530

    Time: 0 ms (100%), Space: 11.5 MB (66.03%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        int i = n-1;
+
+        if(digits[i] != 9){
+            digits[i] += 1;
+        }else{
+            while(i >= 0 && digits[i] == 9){
+                digits[i] = 0;
+                i--;
+            }
+            if(i >= 0){
+                digits[i] += 1;
+            }else{
+                digits.push_back(1);
+                reverse(digits.begin(), digits.end());
+            }
+        }
+
+        return digits;
+    }
+};

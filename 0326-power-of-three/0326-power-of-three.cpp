a9d5b8700304ed61ec4d6adca8e398912f1e9cc2
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-08-13 19:59:48 +0530

    Time: 4 ms (47.44%), Space: 9 MB (14.61%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     
     bool solve(int n){
-        if(n == 1){
-            return true;
-        }
         if(n <= 0){
             return false;
+        }
+        if(n == 1){
+            return true;
         }
 
         if(n % 3 == 0) return solve(n / 3);

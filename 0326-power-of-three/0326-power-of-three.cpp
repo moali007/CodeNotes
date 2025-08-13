365ce7f6009f2fffd205bee030e0453012330fa2
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-08-13 20:00:40 +0530

    Time: 0 ms (100%), Space: 8.8 MB (76.82%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
-    
-    bool solve(int n){
-        if(n <= 0){
-            return false;
-        }
-        if(n == 1){
-            return true;
-        }
-
-        if(n % 3 == 0) return solve(n / 3);
-        return false;
-    }
-
     bool isPowerOfThree(int n) {
-        if(n <= 0) return false;
-
-        return solve(n);
+        return n > 0 && 1162261467 % n == 0;
     }
 };

5cf604f3045eb565dcfbdc8c400f2fb227b88f6c
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-08-31 01:36:10 +0530

    Time: 5 ms (43.76%), Space: 23.3 MB (60.51%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_map<char,int>>rmp(9);
-        vector<unordered_map<char,int>>cmp(9);
-        for(int i=0;i<9;i++)
-        {
-            for(int j=0;j<9;j++)
-            {
-                if(board[i][j]!='.')
-                {
-                    rmp[i][board[i][j]]++;
-                    cmp[j][board[i][j]]++;
-                    if(rmp[i][board[i][j]]>1 ||cmp[j][board[i][j]]>1 )
-                    return false;
-                }
-            }
-        }
-        for(int i=0;i<9;i++)
-        {
-            for(int j=0;j<9;j++)
-            {
-                if(board[i][j]!='.')
-                {
-                    //check in sub matrix
-                    int r=3*(i/3);
-                    int c=3*(j/3);
-                    for(int x=r;x<r+3;x++)
-                    {
-                        for(int y=c;y<c+3;y++)
-                        {
-                            if(x!=i && y!=j)
-                            {
-                                if(board[i][j]==board[x][y])
-                                return false;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        set<pair<int,char>> row,col,grid;
+        for(int i=0;i<9;i++) {  
+            for(int j=0;j<9;j++) {
+                if(board[i][j]!='.') {
+                int count=(i/3)*3 +j/3;
+                
+                if(row.find({i,board[i][j]})==row.end())
+                    row.insert({{i,board[i][j]}});
+                else return false;
 
-        
+                if(col.find({j,board[i][j]})==col.end())
+                    col.insert({{j,board[i][j]}});
+                else return false;
+
+                if(grid.find({count,board[i][j]})==grid.end())
+                    grid.insert({{count,board[i][j]}});
+                else  return false;
+                }
+            }
+        }
         return true;
     }
 };

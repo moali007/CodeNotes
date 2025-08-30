1da5e5b040b4b07093c517a056e89101d4a83645
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2025-08-31 01:45:24 +0530

    Time: 257 ms (5.6%), Space: 101 MB (5.23%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -8,11 +8,11 @@ public:
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(row.find({i,matrix[i][j]})==row.end())
-                    row.insert({{i,matrix[i][j]}});
+                    row.insert({i,matrix[i][j]});
                 else return false;
 
                 if(col.find({j,matrix[i][j]})==col.end())
-                    col.insert({{j,matrix[i][j]}});
+                    col.insert({j,matrix[i][j]});
                 else return false;
             }
         }

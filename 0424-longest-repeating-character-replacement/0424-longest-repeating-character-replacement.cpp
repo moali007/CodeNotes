fa70ea2b3f4318a65ee7026394a6be5975c46328
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-01-13 23:57:08 +0530

    Time: 3 ms (69.49%), Space: 10.8 MB (69.06%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,24 +3,20 @@ public:
     int characterReplacement(string s, int k) {
         int n = s.length();
 
-        vector<int> hash(26, 0);
-        int i=0,j=0;
-        int maxfreq = 0;
-        int maxlen = 0;
+        vector<int> freq(26, 0);
+        int i = 0, j = 0;
+        int maxfreq = 0, maxlen = 0;
 
         while(j < n){
-            hash[s[j] - 'A']++;
-            maxfreq = max(maxfreq, hash[s[j] - 'A']);
+            freq[s[j] - 'A']++;
+            maxfreq = max(maxfreq, freq[s[j] - 'A']);
 
-            if((j-i+1) - maxfreq > k){
-                hash[s[i] - 'A']--;
+            while((j-i+1 - maxfreq) > k){
+                freq[s[i] - 'A']--;
                 i++;
             }
 
-            if((j-i+1) - maxfreq <= k){
-                maxlen = max(maxlen, j-i+1);
-            }
-
+            maxlen = max(maxlen, j-i+1);
             j++;
         }
 

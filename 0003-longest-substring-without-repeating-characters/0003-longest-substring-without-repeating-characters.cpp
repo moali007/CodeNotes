7472680ebcf312ad219b4c2afd2dbe164cf6f4aa
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-04-03 16:03:44 +0530

    Time: 7 ms (64.07%), Space: 11.8 MB (55.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,20 +5,20 @@ public:
 
         int i = 0, j = 0;
         int maxlen = 0;
-
-        unordered_map<char, int> mp;
+        unordered_map<char, int> mp; //{char, idx}
 
         while(j < n){
-            if(!mp.empty() && mp.find(s[j]) != mp.end()){
+            if(!mp.empty() and mp.find(s[j]) != mp.end()){
                 if(mp[s[j]] >= i){
                     i = mp[s[j]] + 1;
                 }
             }
 
             mp[s[j]] = j;
-            maxlen = max(maxlen, j - i + 1);
+            maxlen = max(maxlen, j-i+1);
             j++;
         }
+
         return maxlen;
     }
 };

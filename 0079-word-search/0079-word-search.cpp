4dc8c3ade55216ab2d97a6fcfb901c1ed789e128
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-04-11 20:43:49 +0530

    Time: 867 ms (12.81%), Space: 11.7 MB (30.61%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,51 +1,41 @@
 class Solution {
 public:
-    int m,n;
-    vector<int> delrow = {-1,0,1,0};
-    vector<int> delcol = {0,1,0,-1};
-    // vector<vector<int>> dirs{{-1,0}, {0,1}, {1,0}, {0,-1}};
-    bool find(int row, int col, int idx, string &word, vector<vector<char>>& board){
-        if(idx == word.length()){
+    int m, n;
+    vector<int> drow = {-1, 0, 1, 0};
+    vector<int> dcol = {0, 1, 0, -1};
+
+    bool find(int i, int j, int idx, vector<vector<char>>& board, string word){
+        if(idx == word.size()){
             return true;
         }
 
-        if(row<0 || col<0 || row>=m || col>=n || board[row][col] == '$' || board[row][col] != word[idx]){
+        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '$' || board[i][j] != word[idx]){
             return false;
         }
 
-        //yaha aa gae matlab board[row][col] == word[idx] --> visit board[row][col]
+        char temp = board[i][j];
+        board[i][j] = '$';
 
-        //do
-        char temp = board[row][col];
-        board[row][col] = '$';
+        for(int k = 0; k < 4; k++){
+            int row = i + drow[k];
+            int col = j + dcol[k];
 
-        for(int i=0;i<4;i++){
-            int drow = row + delrow[i];
-            int dcol = col + delcol[i];
-
-            if(find(drow, dcol, idx+1, word, board) == true){
-                return true;
-            }
+            if(find(row, col, idx+1, board, word) == true) 
+              return true;
         }
 
-        //undo
-        board[row][col] = temp;
+        board[i][j] = temp;
         return false;
-    }
 
-    
+    }
 
     bool exist(vector<vector<char>>& board, string word) {
         m = board.size();
         n = board[0].size();
 
-        // int delrow[] = {-1,0,1,0};
-        // int delcol[] = {0,1,0,-1};
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-
-                if(board[i][j] == word[0] && find(i, j, 0, word, board) == true){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word[0] and find(i, j, 0, board, word)){
                     return true;
                 }
             }

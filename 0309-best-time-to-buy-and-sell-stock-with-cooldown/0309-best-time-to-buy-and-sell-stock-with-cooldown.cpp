6e05a23ce0361c502b8220fda5fc481bec2cf868
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-04-15 13:06:57 +0530

    Time: 3 ms (34.72%), Space: 16.3 MB (34.41%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int n;
-    
-    int f(int i, int buy, vector<int>& prices, vector<vector<int>> &dp){
+    int f(int i, int buy, vector<int>&prices, vector<vector<int>>&dp){
         if(i >= n){
             return 0;
         }
-
-        if(dp[i][buy] != -1){
-            return dp[i][buy];
-        }
+        if(dp[i][buy] != -1) return dp[i][buy];
 
         int profit = 0;
         if(buy == 1){
-            //you can buy/not buy on current day
             int bought = -prices[i] + f(i+1, 0, prices, dp);
-            int notBought = f(i+1, 1, prices, dp);
-            profit = max(bought, notBought);
-        }
-        else{
-            //you can sell/not sell on current day
+            int notbuy = f(i+1, 1, prices, dp);
+            profit = max(bought, notbuy);
+        }else{
             int sold = prices[i] + f(i+2, 1, prices, dp);
-            int notSold = f(i+1, 0, prices, dp);
-            profit = max(sold, notSold);
+            int notsld = f(i+1, 0, prices, dp);
+            profit = max(sold, notsld);
         }
 
         return dp[i][buy] = profit;
@@ -31,31 +24,8 @@ public:
     int maxProfit(vector<int>& prices) {
         n = prices.size();
         
-        // vector<vector<int>> dp(n+1, vector<int>(2, -1));
-        // return f(0, 1, prices, dp);
-
-        vector<vector<int>> dp(n+2, vector<int>(2, 0));
-
-        for(int i = n-1 ; i >= 0 ; i--){
-            for(int buy = 0 ; buy <= 1 ; buy++){
-                int profit = 0;
-                if(buy == 1){
-                    //you can buy/not buy on current day
-                    int bought = -prices[i] + dp[i+1][0];
-                    int notBought = dp[i+1][1];
-                    profit = max(bought, notBought);
-                }
-                else{
-                    //you can sell/not sell on current day
-                    int sold = prices[i] + dp[i+2][1];
-                    int notSold = dp[i+1][0];
-                    profit = max(sold, notSold);
-                }
-
-                dp[i][buy] = profit;
-            }
-        }
-
-        return dp[0][1];
+        vector<vector<int>> dp(n+1, vector<int>(3, -1));
+        int buy = 1;
+        return f(0, buy, prices, dp);
     }
 };

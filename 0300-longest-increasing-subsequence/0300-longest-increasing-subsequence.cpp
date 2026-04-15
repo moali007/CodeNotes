49b24c765942225e00c356ec3977249d0529071b
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-04-15 13:21:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
     int n;
-    int f(int i, vector<int>& nums, int prevIdx, vector<vector<int>> &dp){
+    int f(int i, int prev, vector<int>&nums){
         if(i == n){
             return 0;
         }
 
-        if(dp[i][prevIdx + 1] != -1){
-            return dp[i][prevIdx + 1];
-        }
-
-        int skip = f(i+1, nums, prevIdx, dp);
-
+        int nt = f(i+1, prev, nums);
         int take = 0;
-        if(prevIdx == -1 || nums[i] > nums[prevIdx]){
-            take = 1 + f(i+1, nums, i, dp);
+        if(prev == -1 || nums[i] > nums[prev]){
+            take = 1 + f(i+1, i, nums);
         }
 
-        return dp[i][prevIdx + 1] = max(skip, take);
+        return max(nt, take);
     }
 
     int lengthOfLIS(vector<int>& nums) {
         n = nums.size();
-        
-        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        // return f(0, nums, -1, dp);
 
-        vector<int> temp;
-        temp.push_back(nums[0]);
-
-        for(int i = 1 ; i < n ; i++){
-            if(nums[i] > temp.back()){
-                temp.push_back(nums[i]);
-            }
-            else{
-                int idx = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
-                temp[idx] = nums[i];
-            }
-        }
-
-        return temp.size();
+        return f(0, -1, nums);
     }
 };

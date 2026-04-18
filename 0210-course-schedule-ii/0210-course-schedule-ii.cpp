0e062b44d90e809d0d11405935e0bb84b84cc096
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-04-19 00:43:44 +0530

    Time: 7 ms (33.66%), Space: 19.5 MB (8.84%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,43 +2,38 @@ class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
         unordered_map<int, vector<int>> adj;
-        
+
         for(auto it : prerequisites){
-            int u = it[1];
-            int v = it[0];
-            
-            adj[u].push_back(v);
+            int u = it[0];
+            int v = it[1];
+
+            adj[v].push_back(u);
         }
-        
+
         vector<int> indegree(n, 0);
-        
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
             for(auto it : adj[i]){
                 indegree[it]++;
             }
         }
-        
-        vector<int> topo;
-        
+
         queue<int> q;
-        
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
             if(indegree[i] == 0) q.push(i);
         }
-        
+
+        vector<int> topo;
         while(!q.empty()){
-            int node = q.front();
-            q.pop();
+            int node = q.front(); q.pop();
             topo.push_back(node);
-            
+
             for(auto it : adj[node]){
                 indegree[it]--;
-                
                 if(indegree[it] == 0) q.push(it);
             }
         }
         
         if(topo.size() == n) return topo;
-        else return {};
+        return {};
     }
 };

3dab66cf888beaa3309d928c41e27bb816230e43
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-04-20 21:44:52 +0530

    Time: 18 ms (48.98%), Space: 23.8 MB (64.65%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        int n = grid.size();
+        queue<pair<int,int>> q;
+
+        // Step 1: Push all land
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    q.push({i, j});
+                }
+            }
+        }
+
+        // Edge cases
+        if(q.empty() || q.size() == n*n) return -1;
+
+        int dist = -1;
+        vector<int> drow = {-1, 0, 1, 0};
+        vector<int> dcol = {0, 1, 0, -1};
+
+        // Step 2: BFS
+        while(!q.empty()){
+            int sz = q.size();
+            dist++;
+
+            while(sz--){
+                auto [r, c] = q.front();
+                q.pop();
+
+                for(int i = 0; i < 4; i++){
+                    int nr = r + drow[i];
+                    int nc = c + dcol[i];
+
+                    if(nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0){
+                        grid[nr][nc] = 1; // mark visited
+                        q.push({nr, nc});
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
+};

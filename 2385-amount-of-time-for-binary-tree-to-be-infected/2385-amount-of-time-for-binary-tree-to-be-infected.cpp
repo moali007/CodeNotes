9247b1c7e9c2a00852cbb387c39ace72d85d9291
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-04-29 20:47:01 +0530

    Time: 147 ms (83.07%), Space: 196.6 MB (40.3%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    TreeNode* preorder(TreeNode* root, int start){
+        if(root == NULL) return NULL;
+
+        if(root->val == start) return root;
+
+        TreeNode* left = preorder(root->left, start);
+        if(left) return left;
+
+        return preorder(root->right, start);
+    }
+
+    void markParent(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent_track){
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            TreeNode* curr = q.front();
+            q.pop();
+
+            if(curr->left){
+                parent_track[curr->left] = curr;
+                q.push(curr->left);
+            }
+            if(curr->right){
+                parent_track[curr->right] = curr;
+                q.push(curr->right);
+            }
+        }
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+        //get start node
+        TreeNode* startNode = preorder(root, start);
+
+        //mark parents
+        unordered_map<TreeNode*, TreeNode*> parent_track;
+        markParent(root, parent_track);
+
+        unordered_map<TreeNode*, bool> vis;
+        int time = 0;
+        queue<TreeNode*> q;
+        q.push(startNode);
+        vis[startNode] = true;
+
+        while(!q.empty()){
+            int size = q.size();
+            bool spread = false;
+
+            for(int i = 0; i < size; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+
+                if(curr->left and !vis[curr->left]){
+                    spread = true;
+                    vis[curr->left] = true;
+                    q.push(curr->left);
+                }
+                if(curr->right and !vis[curr->right]){
+                    spread = true;
+                    vis[curr->right] = true;
+                    q.push(curr->right);
+                }
+                if(parent_track[curr] and !vis[parent_track[curr]]){
+                    spread = true;
+                    vis[parent_track[curr]] = true;
+                    q.push(parent_track[curr]);
+                }
+            }
+            if(spread == true) time++;
+        }
+        return time;
+    }
+};

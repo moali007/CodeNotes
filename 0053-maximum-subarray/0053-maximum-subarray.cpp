db63f3d6a470fd723934e4c51c8efcaff0a0dff6
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-04-30 22:34:31 +0530

    Time: 0 ms (100%), Space: 71.9 MB (5.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,15 +4,16 @@ public:
         int n = nums.size();
 
         int sum = 0;
-        int maxSum = -1e9;
+        int maxi = -1e9;
 
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
             sum += nums[i];
-            maxSum = max(maxSum, sum);
+
+            maxi = max(maxi, sum);
 
             if(sum < 0) sum = 0;
         }
 
-        return maxSum;
+        return maxi;
     }
 };

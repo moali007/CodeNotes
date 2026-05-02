6bff0274a4d4995163d436a26c26ddf97821c688
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-02 13:33:48 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.19%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    bool isNotSame(int n){
+        bool diff = false;
+        int num = n;
+
+        while(num > 0){
+            int d = num % 10;
+            num = num / 10;
+
+            if(d == 3 || d == 4 || d == 7){
+                return false;
+            }
+            if(d == 2 || d == 5 || d == 6 || d == 9){
+                diff = true;
+                // break;
+            }
+        }
+
+        return (diff == true);
+    }
+
+    int rotatedDigits(int n) {
+        int cnt = 0;
+        for(int i = 0; i <= n; i++){
+            if(isNotSame(i)) cnt++;
+        }
+
+        return cnt;
+    }
+};

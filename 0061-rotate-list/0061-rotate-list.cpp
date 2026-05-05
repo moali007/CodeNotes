b1bf3b86696e647c2d93abea8bf8910bb4c960d3
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-05 22:21:49 +0530

    Time: 0 ms (100%), Space: 16.3 MB (63.05%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -13,32 +13,24 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(head == NULL || head->next == NULL || k == 0) return head;
 
-        //step 1: find length
         int n = 1;
         ListNode* tail = head;
-        
         while(tail->next != NULL){
             tail = tail->next;
             n++;
         }
 
-        //step 2: make circular
         tail->next = head;
 
         k = k % n;
-
-        //step 3: find new tail
         ListNode* newTail = head;
-
         for(int i = 1; i < n-k; i++){
             newTail = newTail->next;
         }
-        
-        //break circular
+
         ListNode* newHead = newTail->next;
         newTail->next = NULL;
 
         return newHead;
-
     }
 };

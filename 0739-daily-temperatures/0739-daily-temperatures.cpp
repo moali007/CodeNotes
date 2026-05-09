3f1262f5544ad730be5651abcbce7bc4ee7f4fb2
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-09 23:04:56 +0530

    Time: 45 ms (5.39%), Space: 106.5 MB (62.58%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
 
-        vector<int> ans(n);
-        stack<pair<int, int>> st; //{temperatures[i], i}
+        vector<int> nge(n);
+        nge[n-1] = 0;
+        stack<pair<int, int>> st;
+        st.push({temp[n-1], n-1});
 
-        for(int i=n-1;i>=0;i--){
-            
-            while(!st.empty() && st.top().first <= temperatures[i]){
+        for(int i = n-2; i >= 0; i--){
+            while(!st.empty() and st.top().first <= temp[i]){
                 st.pop();
             }
 
-            st.empty() ? ans[i] = 0 : ans[i] = st.top().second - i;
+            nge[i] = st.empty() ? 0 : st.top().second;
 
-            st.push({temperatures[i], i});
+            st.push({temp[i], i});
         }
 
-        // reverse(ans.begin(), ans.end());
+        vector<int> ans(n, 0);
+        for(int i = 0; i < n; i++){
+            if(nge[i] == 0) continue;
+            else{
+                ans[i] = nge[i] - i;
+            }
+        }
 
         return ans;
+
     }
 };

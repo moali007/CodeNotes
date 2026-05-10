57aa5cccde5bf9d3845dd5f44ded40ba8020a117
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-10 23:07:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int n;
-
-    int f(int i, vector<int>& nums, int target, vector<int> &dp) {
-        if(i == n - 1) {
+    
+    int f(int i, vector<int>& nums, int target){
+        if(i == n-1){
             return 0;
         }
 
-        if(dp[i] != -1) return dp[i];
-
-        int ans = -1e9;
-
-        for(int j = i + 1; j < n; j++) {
-
-            if(abs(nums[j] - nums[i]) <= target) {
-                int temp = 1 + f(j, nums, target, dp);
+        int ans = INT_MIN;
+        for(int j = i+1; j < n; j++){
+            if(abs(nums[j] - nums[i]) <= target){
+                int temp =  1 + f(j, nums, target);
 
                 ans = max(ans, temp);
             }
         }
 
-        return dp[i] = ans;
+        return ans;
     }
 
     int maximumJumps(vector<int>& nums, int target) {
         n = nums.size();
-        
-        vector<int> dp(n, -1);
-        int ans = f(0, nums, target, dp);
+
+        int ans = f(0, nums, target);
 
         return ans < 0 ? -1 : ans;
     }

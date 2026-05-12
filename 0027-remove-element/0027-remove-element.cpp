803f2e5d0f2a187d9b45bdde3782b1c14d13b21f
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-12 23:32:11 +0530

    Time: 0 ms (100%), Space: 11.7 MB (83.02%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int removeElement(vector<int>& arr, int val) {
-        int n=arr.size();
-        int k=0;
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
 
-        for(int i=0;i<n;i++){
-            if(arr[i] != val){
-                arr[k] = arr[i];
+        int k = 0;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] != val){
+                swap(nums[i], nums[k]);
                 k++;
             }
         }
+        
         return k;
     }
 };

bf77feec3be2bb7db4bbe92e7f21d8d809841c87
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-12 13:15:24 +0530

    Time: 0 ms (100%), Space: 24 MB (18.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,18 +3,12 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        int k=0;
-
-        for(int i=0;i<n;i++){
+        int k = 0;
+        for(int i = 0; i < n; i++){
             if(nums[i] != 0){
-                nums[k] = nums[i];
+                swap(nums[k], nums[i]);
                 k++;
             }
-        }
-
-        while(k < n){
-            nums[k] = 0;
-            k++;
         }
     }
 };

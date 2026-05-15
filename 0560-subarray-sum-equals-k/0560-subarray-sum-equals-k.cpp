b6a97014c94d450e7d817c45fa79de5a0a809b43
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-15 19:58:08 +0530

    Time: 54 ms (37.28%), Space: 51.1 MB (12.59%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,18 +5,15 @@ public:
 
         unordered_map<int, int> mp;
         mp[0] = 1;
-
-        int sum = 0;
+        int preSum = 0;
         int cnt = 0;
 
         for(int i = 0; i < n; i++){
-            sum += nums[i];
+            preSum += nums[i];
 
-            cnt += (mp[sum - k]);
+            cnt += mp[preSum - k];
 
-            // if(sum < 0) sum = 0;
-
-            mp[sum]++;
+            mp[preSum]++;
         }
 
         return cnt;

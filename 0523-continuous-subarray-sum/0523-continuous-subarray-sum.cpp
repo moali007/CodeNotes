6d87293ae0fe2806bc3c437959d57b5fe50eeb6c
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-17 13:22:48 +0530

    Time: 75 ms (78.38%), Space: 146.3 MB (81.13%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,20 +3,24 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
 
-        unordered_map<int, int> mp; //{rem, index they have been seen}
+        unordered_map<int, int> mp;
         mp[0] = -1;
-        int preSum = 0;
+        int presum = 0;
 
         for(int i = 0; i < n; i++){
-            preSum += nums[i];
+            presum += nums[i];
 
-            int rem = preSum % k;
+            int rem = presum % k;
 
             if(mp.count(rem)){
-                if( i - mp[rem] >= 2) return true;
-            }else mp[rem] = i;
+                if(i - mp[rem] >= 2) 
+                   return true;
+            }else{
+                mp[rem] = i;
+            }
         }
 
         return false;
     }
 };
+

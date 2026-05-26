3f09affddf3f4f900da2b412fe0547972499632d
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-05-26 11:55:29 +0530

    Time: 3 ms (29%), Space: 9 MB (78.89%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        sort(word.begin(), word.end());
-        cout<<word<<endl;
-        unordered_map<char, int> mp;
-        int cnt = 0;
+        vector<int> freq(52, 0);
 
         for(char ch : word){
-            if(mp.count(ch - 32)){
-                mp.erase(ch-32);
-                cnt++;
+            if(islower(ch)){
+                freq[ch - 'a']++;
+            }else{
+                freq[ch - 'A' + 26]++;
             }
+        }
 
-            mp[ch]++;
+        int cnt = 0;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] > 0 and freq[i+26] > 0) cnt++;
         }
 
         return cnt;

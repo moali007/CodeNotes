c31ae0565ed58e782cf1a141fccac35fa7c21144
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-06-08 23:17:38 +0530

    Time: 14 ms (13.38%), Space: 127.8 MB (78.53%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -3,29 +3,30 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
 
-        int pc = 0;
-        vector<int> smaller;
-        vector<int> greater;
+        int countLessThanPivot = 0;
+        int countEqualToPivot = 0;
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] < pivot){
-                smaller.push_back(nums[i]);
-            }else if(nums[i] > pivot){
-                greater.push_back(nums[i]);
-            }else{
-                pc++;
+        for(int i=0;i<n;i++){
+            if(nums[i] < pivot) countLessThanPivot++;
+            else if(nums[i] == pivot) countEqualToPivot++;
+        }
+
+        int i=0; //less than pivot iss idx se bhare jaenge
+        int j = countLessThanPivot; //equal to pivot yaha se bhare jaenge
+        int k = countLessThanPivot + countEqualToPivot; //greater than pivot yaha se bhare jange
+
+        vector<int> ans(n);
+        
+        for(int num : nums){
+            if(num < pivot){
+                ans[i++] = num;
             }
-        }
-
-        vector<int> ans;
-        for(int i = 0; i < smaller.size(); i++){
-            ans.push_back(smaller[i]);
-        }
-        while(pc--){
-            ans.push_back(pivot);
-        }
-        for(int i = 0; i < greater.size(); i++){
-            ans.push_back(greater[i]);
+            else if(num == pivot){
+                ans[j++] = num;
+            }
+            else{
+                ans[k++] = num;
+            }
         }
 
         return ans;

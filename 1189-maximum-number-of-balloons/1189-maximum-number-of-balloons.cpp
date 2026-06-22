9c83c242c160b1002acef84bfa943ad5d3ffcf22
Mohammad Ali <123264400+moali007@users.noreply.github.com>
2026-06-22 20:37:16 +0530

    Time: 3 ms (12.33%), Space: 9.2 MB (15.37%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char, int> mp;
+
+        for(char ch : text){
+            mp[ch]++;
+        }
+
+        string s = "balloon";
+
+        int cnt = INT_MAX;
+
+        for(char ch : s){
+            if(mp.find(ch) == mp.end()){
+                return 0;
+            }
+
+            if(ch == 'b' || ch == 'a' || ch == 'n'){
+                cnt = min(cnt, mp[ch]);
+            }
+            else if(ch == 'o' || ch == 'l'){
+                cnt = min(cnt, mp[ch] / 2);
+            }
+        }
+
+        return cnt;
+    }
+};
+
